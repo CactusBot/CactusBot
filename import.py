0064b6bbe3f53164a072104b3cac782dd867cb09
pingpong1109 <jeremiahtext@gmail.com>
2016-04-21 14:46:34 -0400

    added list to import for launch

--- import.py
+++ import.py
@@ -1,1 +1,32 @@
 """SoonTM to import commands/points/quotes and such from other bots"""
+
+import requests
+
+class Imports():
+
+    def __init__(self):
+        pass
+
+    def __exit__(self):
+        pass
+
+    def import_nightbot(self):
+        pass
+
+    def import_moobot(self):
+        pass
+
+    def import_deepbot(self):
+        pass
+
+    def import_benbot(self):
+        pass
+
+    def import_anhkbot(self):
+        pass
+
+    def import_scottybot(self):
+        pass
+
+    def import_revlobot(self):
+        pass
