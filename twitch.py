87ec71a11471d459ec33c0883e54627fda731800
pingpong1109 <jeremiahtext@gmail.com>
2016-05-13 20:09:58 -0400

    TWITCH BETA IS DONE!

--- twitch.py
+++ twitch.py
@@ -19,7 +19,7 @@ class Twitch():
 
     def __init__(self, debug="INFO", **kwargs):
         self._init_logger(debug, kwargs.get("log_to_file", True))
-        self.http_session = Session()
+        self.twitch_login = requests.Session()
 
         # Change these to however you guys are storing data
         with open('data/bots/bot.json', "r+") as config:
@@ -34,6 +34,10 @@ class Twitch():
         self.channel = bot["twitch"]["channel"]
         self.username = bot["twitch"]["username"]
         self.password = bot["twitch"]["password"]
+
+        # Gets the channel id from the channel name
+        temp = self.twitch_login.get("https://api.twitch.tv/kraken/channels/{channel}".format(channel=self.channel))
+        self.channel_id = temp[channel_id]
 
         # Creates socket. Leave this alone unless you know the socket module *cough* cubed *cough* not using websockets *cough*
         self.twitch_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -93,7 +97,6 @@ class Twitch():
         self.twitch_socket.send(bytes("CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership" + "\n", "utf-8"))
         self.twitch_socket.send(bytes("JOIN #" + self.channel + "\n", "utf-8"))
 
-        self.twitch_login = requests.Session()
         self.twitch_login.post("https://api.twitch.tv/kraken?oauth_token={password}".format(password=self.password))
         #self.send_message("OHAI")
         #self.get_users()
@@ -123,37 +126,68 @@ class Twitch():
         """Use this to send a message, supports /commands"""
         self.twitch_socket.send(bytes("PRIVMSG #{channel} :".format(channel=self.channel) + message + "\n", "utf-8"))
 
-    def get_users(self):
-        """Gets all users in chat, no offical API so need to do this over IRC"""
-        print(self.send_message("/NAMES #{channel}".format(channel=self.channel)))
-
     def get_followers(self):
         """Returns the latest follower"""
-        pass
-        #while True:
-        #    follows = self.twitch_login.get("https://api.twitch.tv/kraken/channels/{channel}/follows".format(channel=self.channel))
-        #    follow = follows.json()
-        #    #print(follow)
-        #    #newest = follow["created_at"]
-        #    #second = follow["created_at"]
-        #    if second == newest:
-        #        return follow[0][user][display_name]
-        #    time.sleep(.1)
+        follows = self.twitch_login.get("https://api.twitch.tv/kraken/channels/{channel}/follows".format(channel=self.channel))
+        while True:
+            newest = follows[0][user][_id]
+            follows = self.twitch_login.get("https://api.twitch.tv/kraken/channels/{channel}/follows".format(channel=self.channel))
+            second = follows[1][user][_id]
+            if second == newest:
+                return str(follow[0][user][display_name])
+            time.sleep(.1)
 
     def get_hosts(self):
-        pass
+        """Grabs latest host from Twitch"""
+        hosts = self.twitch_login.get("https://tmi.twitch.tv/hosts?include_logins=1&target={channel_id}".format(channel_id=self.channel_id))
+        while True:
+            newest = hosts[0][host_id]
+            hosts = self.twitch_login.get("https://tmi.twitch.tv/hosts?include_logins=1&target={channel_id}".format(channel_id=self.channel_id))
+            second = hosts[1][host_id]
+            if second == newest:
+                return str(hosts[0][host_login])
+            time.sleep(.1)
 
     def get_viewers(self):
         """Grabs all viewers in chat"""
-        pass
         viewers = self.twitch_login.get("https://tmi.twitch.tv/group/user/{channel}/chatters".format(channel=self.channel))
         return str(viewers[chatters][viewers])
 
+    def get_moderators(self):
+        """Grabs all moderators in chat"""
+        moderators = self.twitch_login.get("https://tmi.twitch.tv/group/user/{channel}/chatters".format(channel=self.channel))
+        return str(moderators[chatters][moderators])
+
+    def get_global_mods(self):
+        """Grabs all global_mods in chat"""
+        global_mods = self.twitch_login.get("https://tmi.twitch.tv/group/user/{channel}/chatters".format(channel=self.channel))
+        return str(global_mods[chatters][global_mods])
+
+    def get_admins(self):
+        """Grabs all admins in chat"""
+        admins = self.twitch_login.get("https://tmi.twitch.tv/group/user/{channel}/chatters".format(channel=self.channel))
+        return str(admins[chatters][admins])
+
+    def get_staff(self):
+        """Grabs all staff in chat"""
+        staff = self.twitch_login.get("https://tmi.twitch.tv/group/user/{channel}/chatters".format(channel=self.channel))
+        return str(staff[chatters][staff])
+
+    def get_all_viewers(self):
+        """Grabs all viewers in chat"""
+        all_viewers = self.twitch_login.get("https://tmi.twitch.tv/group/user/{channel}/chatters".format(channel=self.channel))
+        return str(all_viewers[chatters][viewers]), str(all_viewers[chatters][moderators]), str(all_viewers[chatters][global_mods]), str(all_viewers[chatters][admins]), str(all_viewers[chatters][staff])
+
     def get_subs(self):
         """Gets the subs for a channel"""
-        pass
         subs = self.twitch_login.get("https://api.twitch.tv/kraken/channels/{channel}/subscriptions".format(channel=self.channel))
-        return str(subs["subscriptions"]["user"]["name"])
+        while True:
+            newest = subs[0][host_id]
+            subs = self.twitch_login.get("https://api.twitch.tv/kraken/channels/{channel}/subscriptions".format(channel=self.channel))
+            second = subs[1][host_id]
+            if second == newest:
+                return str(subs["subscriptions"]["user"]["name"])
+            time.sleep(.1)
 
     def start_commercial(self, length):
         """Runs a commercial with the specified length"""
