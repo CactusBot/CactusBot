696459091f38a0558021cc4c69931c52b6b925a1
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-10-30 17:14:35 -0400

    Added the MIT license to the start of every file Because Hacktoberfest and I needed 1 more pullreq :P

--- cactusbot/commands/command.py
+++ cactusbot/commands/command.py
@@ -1,5 +1,29 @@
 """The inner workings of the command system."""
 
+"""
+The MIT License
+
+Copyright (c) 2016 CactusDev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
 from inspect import signature
 
 from functools import wraps
@@ -132,7 +156,8 @@ class Command(metaclass=CommandMeta):
                         if isinstance(annotation, str):
                             if annotation.startswith('?'):
                                 annotation = self.REGEX.get(annotation[1:], '')
-                            match = re.match('^'+annotation+'$', args[index])
+                            match = re.match(
+                                '^' + annotation + '$', args[index])
                             if match is None:
                                 return "Invalid {type}: '{value}'.".format(
                                     type=argument.name, value=args[index])
